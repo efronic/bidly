c5479d5c53f80af42cb52cea027e64e77318b691
efron <eahmadifar@outlook.com>
2024-06-13 14:37:40 +1000

    chore: Bringing shadcn and using their components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from 'next';
-import { Inter as FontSans } from 'next/font/google';
+import { Dosis as FontDosis } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
 
-const fontSans = FontSans({
+const fontDosis = FontDosis({
   subsets: ['latin'],
-  variable: '--font-sans',
+  variable: '--font-dosis',
 });
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body
         className={cn(
           'min-h-screen bg-background font-sans antialiased',
-          fontSans.variable
+          fontDosis.variable
         )}
       >
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image';
 import { database } from '@/app/db/database';
 import { bids as bidsSchema } from '@/app/db/schema';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { revalidatePath } from 'next/cache';
 
 export default async function HomePage() {
   const bids = await database.query.bids.findMany();
@@ -11,10 +14,12 @@ export default async function HomePage() {
           'use server';
           const name = formData.get('name') as string;
           await database.insert(bidsSchema).values({ name }).execute();
+          revalidatePath('/');
         }}
+        className='flex space-x-4'
       >
-        <input type='text' name='name' />
-        <button type='submit'>Submit</button>
+        <Input type='text' name='name' />
+        <Button type='submit'>Submit</Button>
       </form>
       {bids.map((bid) => (
         <div key={bid.id}>
