f57156b4838bb7d5496f2c8f864dc19e17f39de7
efron <eahmadifar@outlook.com>
2024-06-17 20:40:31 +1000

    chore: Update database schema and dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,36 @@
 import Image from 'next/image';
 import { database } from '@/app/db/database';
-import { bids as bidsSchema } from '@/app/db/schema';
+import { items } from '@/app/db/schema';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { revalidatePath } from 'next/cache';
-import { auth } from '@/auth';
-import { LoginLink, LogoutLink } from '@kinde-oss/kinde-auth-nextjs/components';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
 export default async function HomePage() {
-  const bids = await database.query.bids.findMany();
-  const session = await auth();
+  const allItems = await database.query.items.findMany();
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (!user) return null;
   return (
     <main className=' container mx-auto py-12'>
       <form
         action={async (formData: FormData) => {
           'use server';
           const name = formData.get('name') as string;
-          await database.insert(bidsSchema).values({ name }).execute();
+          await database
+            .insert(items)
+            .values({ name, userId: user?.id })
+            .execute();
           revalidatePath('/');
         }}
         className='flex space-x-4'
       >
-        <Input type='text' name='name' />
+        <Input name='name' placeholder='Name your item'/>
         <Button type='submit'>Submit</Button>
       </form>
-      {bids.map((bid) => (
-        <div key={bid.id}>
-          <p>{bid.name}</p>
+      {allItems.map((item) => (
+        <div key={item.id}>
+          <p>{item.name}</p>
         </div>
       ))}
     </main>
